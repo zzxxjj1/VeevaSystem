298a363c02212e5ac1b79d5c8ee8188a39377776
Stephen Zheng <xzheng97@gmail.com>
2021-11-06 20:03:22 -0400

    Get selectbox working

--- Veeva_System.py
+++ Veeva_System.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
 
 st.title("""Veeva System""")
 prescriber_df = pd.read_csv("Prescriber_Data.csv")
@@ -12,3 +13,34 @@ doctors_selectbox = st.selectbox(
     doctor_names
 )
 #st.write('Doctor: ', doctors_selectbox)
+def locate_row(name):
+    first_name, last_name = name.split()
+    for row in prescriber_df.iterrows():
+        if (first_name == row[1][1]) and (last_name == row[1][2]):
+            return row[1][0]
+
+
+
+
+def make_hist(id):
+  doctor_NRx = prescriber_df.iloc[id-1, 5:11]
+  doctor_NRx = doctor_NRx.to_numpy()
+  doctor_TRx = prescriber_df.iloc[id-1, 11:]
+  doctor_TRx = doctor_TRx.to_numpy()
+  month = ['Month_1', 'Month_2', 'Month_3', 'Month_4', 'Month_5', 'Month_6']
+  fig = go.Figure(
+      data = [
+              go.Bar(name = 'NRx', x = month, y = doctor_NRx, yaxis='y', offsetgroup = 1),
+              go.Bar(name = 'TRx', x = month, y = doctor_TRx, yaxis='y2', offsetgroup =2)
+      ],
+      layout ={
+          'yaxis' : {'title': 'NRx axis'},
+          'yaxis2' : {'title' :'TRx axis', 'overlaying': 'y', 'side': 'right'}
+      }
+  )
+  fig.update_layout(barmode='group')
+  return fig
+
+index = locate_row(doctors_selectbox)
+doctor_fig = make_hist(index)
+st.plotly_chart(doctor_fig, use_container_width=True)
